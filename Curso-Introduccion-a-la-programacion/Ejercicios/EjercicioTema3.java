51399a50544b2f6b28b94ef27ecb0d2ced5ce2d9
Alan Cejas <cejasalandavid01@gmail.com>
2022-11-08 11:35:21 -0300

    Ejercicio 3 curso introducción

---
+++ Curso-Introduccion-a-la-programacion/Ejercicios/EjercicioTema3.java
@@ -0,0 +1,30 @@
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 30;
+        int num3 = 50;
+
+        var valor = sumaTres(num1, num2, num3);
+        System.out.println(valor);
+
+        Coche miCoche = new Coche();
+        miCoche.aumentarPuertas();
+        miCoche.aumentarPuertas();
+        miCoche.aumentarPuertas();
+        miCoche.aumentarPuertas();
+
+        System.out.println("El auto tiene " + miCoche.puertas + " puertas");
+    }
+    public static int sumaTres(int a, int b, int c){
+        return a + b + c;
+    }
+}
+
+class Coche {
+    public int puertas;
+
+    public void aumentarPuertas(){
+        this.puertas++;
+    }
+
+}
